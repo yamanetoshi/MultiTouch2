957e1235958aff25313d06b7a286174cce913385
yamanetoshi <yamanetoshi@gmail.com>
2011-11-23 19:49:17 +0900

    initial commit

---
+++ src/jp/shuri/android/mt/MultiTouch.java
@@ -0,0 +1,13 @@
+package jp.shuri.android.mt;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MultiTouch extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
